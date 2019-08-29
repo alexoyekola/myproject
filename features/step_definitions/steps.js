d4832e69985994f4d960ca897e4ca5eb2fb5a5f5
alexoyekola@gmail.com <alexoyekola@gmail.com>
2019-08-29 11:56:48 +0100

    commit_290819_1

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,7 +1,7 @@
 const { client } = require('nightwatch-cucumber')
 const { After, Before, Given, Then, When } = require('cucumber')
-const assert = require('assert');
-const dragAndDrop = require('html-dnd').codeForSelectors;
+var assert = require('assert');
+const { dragAndDrop } = require('html-dnd').codeForSelectors;
 var Page = client.page.PageObjects()
 var MyTDate = new Date(); var dd = MyTDate.getDate(); var mm = Number(MyTDate.getMonth())+1; var yyyy = MyTDate.getFullYear();
 if(dd<10){ dd='0'+dd }
@@ -12,7 +12,6 @@ var FirstTime = 1
 Before("@BaseURL", () => {
     client.url(client.launch_url)
 })
-
 // Login Testing
 Given(/^User is on the login screen$/, () => {  
     return Page.waitForElementVisible('@UsernameInput')
@@ -25,7 +24,7 @@ When(/^User enters username: "([^"]*)" and password: "([^"]*)"$/, (username,pass
     Page.waitForElementVisible('@PasswordInput').setValue('@PasswordInput', 'Dragonfly1!')
     return Page.click('@LoginBtn')
 })
-Then(/^User successfully logged into Forsight/, () => {
+Then(/^User successfully logged into Forsight$/, () => {
     return Page.waitForElementVisible('@Logo')
 })
 Then(/^User receives the error messages "([^"]*)" or "([^"]*)"$/, (usernameerror, passworderror) => {
@@ -38,7 +37,8 @@ Then(/^User receives the error messages "([^"]*)" or "([^"]*)"$/, (usernameerror
     }
 })
 
-Given(/^User is on the home page/, () => {
+Given(/^User is on the home page$/, () => {
+    //User logged in and is on the home page
     Page.waitForElementVisible('@UsernameInput').setValue('@UsernameInput', 'alexoyekola@piccadillygroup.com')
     Page.waitForElementVisible('@ConfirmUsername').click('@ConfirmUsername')
     Page.waitForElementVisible('@PasswordInput').setValue('@PasswordInput', 'Dragonfly1!')
@@ -46,26 +46,28 @@ Given(/^User is on the home page/, () => {
     return Page.waitForElementVisible('@Logo')
 })
 //Designs Screen
-When(/^User navigates to design screen/, () => {
+When(/^User navigates to design screen$/, () => {
+    
     Page.waitForElementVisible('@ProcessManagementIcon', 30000)
-    Page.click('@ProcessManagementIcon')
+    Page.click('@ProcessManagementIcon')    //navigating to the menu bar and selecting a screen
     client.pause(1000)
     Page.waitForElementVisible('@ProcessManagementText')
     Page.waitForElementVisible('@DesignsIcon', 30000)
     Page.click('@DesignsIcon')
     return Page.click('@DesignsIcon')
 })
-Then(/^User inputs design details and saves/, () => {
+Then(/^User inputs design details and saves$/, () => {
     Page.waitForElementVisible('@AddDesignBtn').click('@AddDesignBtn')
     Page.waitForElementVisible('@DesignNameInput').setValue('@DesignNameInput', 'test_design_alex')
     Page.waitForElementVisible('@DescriptionBox').click('@DescriptionBox').setValue('@DescriptionBox', 'this is a test')
+    // Whilst creating a new design inputting the details of its name, description and values
     client.pause(10000)
     return Page.waitForElementVisible('@Create_DesignBtn').click('@Create_DesignBtn') 
 })
-Then(/^User successfully created new design/, () => {
+Then(/^User successfully created new design$/, () => {
     return Page.waitForElementVisible('@DesignUpdatedPopUp')
 })
-Then(/^User adds activities/, () => {
+Then(/^User adds activities$/, () => {
     Page.waitForElementVisible('@NewActivityBtn').click('@NewActivityBtn')
     client.pause(5000)
     Page.click('@AddActivityBtn')
@@ -80,7 +82,7 @@ Then(/^User adds activities/, () => {
     client.pause(15000)
     return Page.waitForElementVisible('@SaveBtn').click('@SaveBtn')
 })
-Then(/^User updates upload activities/, () => {
+Then(/^User updates upload activities$/, () => {
     Page.waitForElementVisible('@UploadTableDataActivity').click('@UploadTableDataActivity')
     client.pause(10000)
     Page.waitForElementVisible('@UploadSampleBtn').click('@UploadSampleBtn')
@@ -99,7 +101,7 @@ Then(/^User updates upload activities/, () => {
     return client.pause(3000)
 })
 //Entities Screen
-Given(/^User is on the entities screen/, () => {
+Given(/^User is on the entities screen$/, () => {
     Page.waitForElementVisible('@UsernameInput').setValue('@UsernameInput', 'alexanderoyekola@piccadillygroup.com')
     Page.waitForElementVisible('@ConfirmUsername').click('@ConfirmUsername')
     Page.waitForElementVisible('@PasswordInput').setValue('@PasswordInput', 'Dragonfly1!')
@@ -111,18 +113,18 @@ Given(/^User is on the entities screen/, () => {
     Page.waitForElementVisible('@EntitiesIcon', 30000).click('@EntitiesIcon')
     return Page.click('@EntitiesIcon')
 })
-When(/^User creates new entity/, () => {
+When(/^User creates new entity$/, () => {
     Page.waitForElementVisible('@NewEntityBtn').click('@NewEntityBtn')
     Page.click('@NewEntityBtn')
     Page.waitForElementVisible('@NewEntityNameInput').setValue('@NewEntityNameInput', 'Test_Entity')
     Page.waitForElementVisible('@NewEntitySaveBtn')
     return Page.click('@NewEntitySaveBtn')
 })
-Then(/^User has successfully created new entity/, () => {
+Then(/^User has successfully created new entity$/, () => {
     Page.waitForElementVisible('@EntitiesTitle')
     return client.pause(2000)
 })
-When(/^User copies an entity/, () => {
+When(/^User copies an entity$/, () => {
     Page.waitForElementVisible('@EntityDetailsBtn').click('@EntityDetailsBtn')
     client.pause(1000)
     Page.waitForElementVisible('@CopyEntityBtn').click('@CopyEntityBtn')
@@ -131,20 +133,20 @@ When(/^User copies an entity/, () => {
     Page.waitForElementVisible('@CopyBtn')
     return Page.click('@CopyBtn')
 })
-Then(/^User has successfully copied an entity/, () => {
+Then(/^User has successfully copied an entity$/, () => {
     return Page.waitForElementVisible('@SuccessfulMessage')
 })
-When(/^User selects to hide an entity/, () => {
+When(/^User selects to hide an entity$/, () => {
     Page.waitForElementVisible('@EntityDetailsBtn').click('@EntityDetailsBtn')
     Page.waitForElementVisible('@HideEntityBtn')
     client.pause(1000)
     return Page.click('@HideEntityBtn')
 })
-Then(/^User has successfully hidden an entity/, () => {
+Then(/^User has successfully hidden an entity$/, () => {
     return Page.waitForElementVisible('@SuccessfulMessage')
 })
 //Periods screen 
-Given(/^User is on the periods screen/, () => {
+Given(/^User is on the periods screen$/, () => {
     Page.waitForElementVisible('@UsernameInput').setValue('@UsernameInput', 'alexanderoyekola@piccadillygroup.com')
     Page.waitForElementVisible('@ConfirmUsername').click('@ConfirmUsername')
     Page.waitForElementVisible('@PasswordInput').setValue('@PasswordInput', 'Dragonfly1!')
@@ -156,7 +158,7 @@ Given(/^User is on the periods screen/, () => {
     Page.waitForElementVisible('@PeriodsIcon', 30000).click('@PeriodsIcon')
     return Page.click('@PeriodsIcon')
 })
-When(/^User creates new period/, () => {
+When(/^User creates new period$/, () => {
     Page.waitForElementVisible('@NewPeriodBtn').click('@NewPeriodBtn')
     Page.waitForElementVisible('@NewPeriodNameInput').setValue('@NewPeriodNameInput', 'test_Monthly')
     client.pause(5000)
@@ -167,18 +169,34 @@ When(/^User creates new period/, () => {
     Page.waitForElementVisible('@NewPeriodSaveBtn')
     return Page.click('@NewPeriodSaveBtn')
 })
-Then(/^User has successfully created new period/, () => {
+Then(/^User has successfully created new period$/, () => {
     return Page.waitForElementVisible('@SuccessfulMessage')
 })
-Given(/^User is on page/, () => {
-    return client.url('https://www.w3schools.com/html/html5_draganddrop.asp')
+Given(/^User is on page$/, () => {
+    return client.url("http://jqueryui.com/resources/demos/draggable/default.html")
 })
-Then(/^Drag and drop success/, () => {
-    Page.waitForElementVisible('@EleFrom')
-    Page.moveToElement('#drag1', 5, 5)
+
+//TEST TO IMPLEMENT DRAG AND DROP
+Then(/^Drag and drop success$/, () => {
+    client.useCss()
+    Page.waitForElementVisible('#draggable', 5000);
+    client.pause(5000)
+    Page.moveToElement('#draggable', 0, 0)
     client.mouseButtonDown(0)
-    Page.moveToElement('#div2', 5, 5)
-    client.pause(3000)
+    Page.moveToElement('body', 200, 600) // Move to offset position of 200(x) 600(y)
     client.mouseButtonUp(0)
-    return client.pause(3000)
+    return client.pause(5000)  // Keep browser open for 10 seconds so you can see result
 })
+
+//DRAG AND DROP ON AN ACTIVITY
+/* 
+client.useXpath()
+Page.waitForElementVisible('//*[@id="activity-diagram-component"]/svg/g/g[2]/g[7]/rect', 10000)
+client.pause(3000)
+Page.moveToElement('//*[@id="activity-diagram-component"]/svg/g/g[2]/g[7]/rect', 0, 0)
+client.mouseButtonDown(0)
+Page.moveToElement('//*[@id="activity-diagram-component"]/svg', 40, 130)
+client.pause(3000)
+client.mouseButtonUp(0)
+return client.pause(5000)
+*/
